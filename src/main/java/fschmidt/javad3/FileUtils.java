8fc41b459095e3bf3a700ec36c6aa5694afc1d88
Florian Schmidt <florian.schmidt@tu-berlin.de>
2018-12-03 19:30:00 +0100

    bugfix copy files from classpath

--- src/main/java/fschmidt/javad3/FileUtils.java
+++ src/main/java/fschmidt/javad3/FileUtils.java
@@ -2,10 +2,11 @@ package fschmidt.javad3;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 
 /**
  *
@@ -14,7 +15,9 @@ import java.nio.file.Files;
 public class FileUtils {
 
     public void copyFromJar(String resource, String endPath) throws URISyntaxException, FileNotFoundException, IOException{
-        File newFile = new File(getClass().getClassLoader().getResource(resource).toURI());
-        Files.copy(newFile.toPath(), new FileOutputStream(new File(endPath+"/"+resource)));
+//        File newFile = new File(getClass().getClassLoader().getResource(resource).toURI());
+//        Files.copy(newFile.toPath(), new FileOutputStream(new File(endPath+"/"+resource)));
+         InputStream newFile = getClass().getClassLoader().getResourceAsStream(resource);
+        Files.copy(newFile, new File(endPath+"/"+resource).toPath());
     }
 }
